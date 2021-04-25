3b263d7c5d54eb2e4501a22eb9d1e03aaf319477
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-04-25 18:43:28 +0700

    Implement Boyer-Moore algorithm

--- src/stringmatching.js
+++ src/stringmatching.js
@@ -6,7 +6,7 @@ const generateReply = (query) => {
  * Knuth-Morris-Pratt algorithm untuk pattern matching
  * @param {string} string string yang ingin dicari patternnya
  * @param {string} pattern pattern yang ingin dicari
- * @returns {boolean} apakah ada pattern dalam string
+ * @returns {int} index kemunculan pertama pattern, -1 jika tidak ditemukan
  */
 const KMP = (string, pattern) => {
   const b = (x) => {}; // border function
@@ -17,11 +17,30 @@ const KMP = (string, pattern) => {
  * Boyer-Moore algorithm untuk pattern matching
  * @param {string} string string yang ingin dicari patternnya
  * @param {string} pattern pattern yang ingin dicari
- * @returns {boolean} apakah ada pattern dalam string
+ * @returns {int} index kemunculan pertama pattern, -1 jika tidak ditemukan
  */
 const BoyerMoore = (string, pattern) => {
-  const L = (x) => {}; // last occurrence function
-  return true;
+  const LMap = pattern.split('').reduce((map, x, i) => {
+    map[x] = i;
+    return map;
+  }, {});
+  const L = (x) => LMap[x] ?? -1; // last occurrence function
+  let n = string.length, m = pattern.length;
+  let i = j = m-1;
+  while(i < n){ 
+    if(string.charAt(i) == pattern.charAt(j)){
+      if(j == 0) return i;
+      else{
+        i--; j--;
+      }
+    }
+    else{
+      let lo = L(string.charAt(i));
+      i = i + m - Math.min(j, lo+1);
+      j = m - 1;
+    }
+  }
+  return -1;
 }
 
 module.exports = { generateReply };
